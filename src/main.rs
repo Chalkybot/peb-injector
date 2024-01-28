535acc05c522a197498e29b5b563ec0a5838ba8e
Chalkybot <chalkbot@pm.me>
2024-01-28 18:20:42 +0200

    Reshuffled main and created dedicated functions. Casting PEB to PEB struct

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,26 @@ use windows::Win32::System::Threading::{CreateProcessA,
                                         PROCESS_INFORMATION,
                                         OpenProcess,
                                         PROCESS_ACCESS_RIGHTS,
-                                        PROCESS_BASIC_INFORMATION};
+                                        PROCESS_BASIC_INFORMATION,
+                                        PEB,
+                                        RTL_USER_PROCESS_PARAMETERS};
 use windows::Wdk::System::Threading::{NtQueryInformationProcess,
                                       PROCESSINFOCLASS,
                                       };
 use windows::Win32::System::Diagnostics::Debug::ReadProcessMemory;
 use windows::Win32::Foundation::GetLastError;
-use windows::Win32::Foundation::BOOL;
+use windows::Win32::Foundation::{BOOL, HANDLE};
 use windows::core::{PSTR, PCSTR};
 use core::ffi::c_void;
 
-
-
-
-fn main() {
-    const SUSPENDED: u32 = 0x00000004;
-    const PROCESS_VM_READ: u32 = 0xFFFF; // placeholder, correct rights are 0x0020 + whatever ntquery requires.
-    let process_access_rights = PROCESS_ACCESS_RIGHTS(PROCESS_VM_READ);
-    let mem_block = std::mem::size_of::<PROCESS_BASIC_INFORMATION>();
-    unsafe {
-    let mut startup_info = STARTUPINFOA::default();
-        let mut process_information = PROCESS_INFORMATION::default();
-
-        let _result = CreateProcessA(
+unsafe fn start_powershell(binary_name: &str, commandline_args: &str, 
+                        process_information: &mut PROCESS_INFORMATION, 
+                        startup_information: &STARTUPINFOA ) -> Result<(),()>{
+    
+    let process_name = format!("{binary_name} {commandline_args}\0");
+    let started_process = CreateProcessA(
             PCSTR::null(),      
-            PSTR("powershell sleep 10\0".as_ptr() as _ ),   // Command line
+            PSTR(process_name.as_str().as_ptr() as _ ),   // Command line
             None,                                           // Process security attributes
             None,                                           // Thread security attributes
             BOOL(0),                                        // Inherit handles
@@ -37,64 +32,92 @@ fn main() {
                                                             // show up in taskmgr?
             None,                                           // Environment
             None,                                           // Current directory
-            &mut startup_info,
-            &mut process_information,
+            startup_information as *const _,
+            process_information as *mut _,
+    );
+    match started_process {
+        Ok(t) => return Ok(()),
+        Err(_) => return Err(())
+    }
+}
+
+unsafe fn read_process_memory(process_handle: HANDLE, address: *const c_void,
+                              buffer_ptr: *mut c_void, length: usize, bytes_read: &mut usize) -> Result<(),()> {
+    let process_memory = ReadProcessMemory(
+                process_handle,
+                address,
+                buffer_ptr,
+                length,
+                Some(bytes_read),
         );
-        let id = process_information.dwProcessId;
-        
+    match process_memory.is_ok() {
+            true  => return Ok(()),
+            false => return Err(())
+    }       
+}
+ 
+
+fn main() {
+    const SUSPENDED: u32 = 0x00000004;
+    const PROCESS_VM_READ: u32 = 0xFFFF; // placeholder, correct rights are 0x0020 + whatever ntquery requires.
+    let process_access_rights = PROCESS_ACCESS_RIGHTS(PROCESS_VM_READ);
+    let mem_len = std::mem::size_of::<PROCESS_BASIC_INFORMATION>();
+    
+    unsafe {
+        let mut startup_info = STARTUPINFOA::default();
+        let mut process_information = PROCESS_INFORMATION::default();
+        start_powershell("powershell", "sleep 10", &mut process_information, &startup_info);
+
+        let id = process_information.dwProcessId; 
         println!("Process ID: {:?}", id);
+
         // Let's open a process handle:
         let process_handle = OpenProcess(
             process_access_rights, // [in] DWORD dwDesiredAccess,
             BOOL(0),    // [in] BOOL  bInheritHandle,
             id          // [in] DWORD dwProcessId
         );
-        let process_info_class = PROCESSINFOCLASS(0); // Should return the location of the PEB block.
-        let mut process_information = vec![0u8; mem_block]; 
 
-        let lp_buffer = process_information.as_mut_ptr() as *mut c_void;
+        let process_info_class = PROCESSINFOCLASS(0); // Should return the location of the PEB block.
+        let mut process_information = PROCESS_BASIC_INFORMATION::default();
+        let lp_buffer: *mut c_void = &mut process_information as *mut _ as *mut c_void;
         let return_length: *mut u32 = std::ptr::null_mut();
         
         let status = NtQueryInformationProcess(
                 process_handle.clone().unwrap(),    //  [in] HANDLE ProcessHandle,
                 process_info_class,                 //  [in] PROCESSINFOCLASS ProcessInformationClass,
                 lp_buffer,                          //  [out] PVOID ProcessInformation,
-                process_information.len() as u32,   //  [in] ULONG ProcessInformationLength,
+                mem_len as u32,   //  [in] ULONG ProcessInformationLength,
                 return_length,                      //  [out, optional] PULONG ReturnLength
         );
 
         match status.is_ok() {
             true => {
-                let _x = PROCESS_BASIC_INFORMATION(lp_buffer);
-                println!("Process basic information: {:?}", _x);
-                println!("Location of PEB: 0x{:?}", &process_information[1]);
-                println!("Returned block: \n{:?}", &process_information);
-
+                println!("Location of PEB: {:?}", &process_information.PebBaseAddress);
         },
             false => eprintln!("Failure: {:?}", status)
         }
-        let peb_loc = process_information[1] as *const c_void;
+       
+        // Reading PEB
+        let peb_addr: *const c_void = process_information.PebBaseAddress as *const _ as *const c_void;
+        let mut peb = PEB::default();
+        let mut buffer_ptr = &mut peb as *mut _ as *mut c_void;
+        let peb_len = std::mem::size_of::<PEB>();
+        let mut bytes_read: usize = 0;
         
         
-        let base_address = lp_buffer;
-        let mut buffer: [u8; 512] = [0; 512]; 
-        let memory_buffer = buffer.as_mut_ptr() as *mut c_void;
-        let mut number_of_bytes_read: usize = 0;
-
-        let process_memory = ReadProcessMemory(
-                process_handle.unwrap(),
-                base_address,
-                memory_buffer,
-                buffer.len(),
-                Some(&mut number_of_bytes_read as *mut usize),
-        );
-        match process_memory.is_ok() {
-            true => println!("Contents of PEB: {:?}", process_memory),
-            false =>  {
-                eprintln!("Requested {:?} bytes, got {:?} back.\nFailure: {:?}", buffer.len(), number_of_bytes_read, process_memory);
-                //eprintln!("Buffer:\n{:?}", buffer);
-            }
+        let peb_content = read_process_memory(process_handle.clone().unwrap(),
+                                                 peb_addr,
+                                                 buffer_ptr,
+                                                 peb_len,
+                                                 &mut bytes_read); 
+        match peb_content {
+            Ok(_) => println!("Success."),
+            Err(_) => eprintln!("Failure")
         }
+
+
+ 
 
         
     }
